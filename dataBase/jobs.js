b5dcf52ca88a5728fd12d6554a43681633a4df90
juanHerrera <portador1051@gmail.com>
2024-06-18 00:52:13 -0400

    Despligue hacia github pages v1.2.1 --Bug

--- dataBase/jobs.js
+++ dataBase/jobs.js
@@ -1,38 +1,41 @@
 export const jobs = [
   {
-    id: '1',
-    name: 'Bodegal',
-    order: 141541,
-    date: '2014-12-12',
-    star: '08:30',
-    end: '09:30',
-    total_horas: '2.00'
-  },
-  {
-    id: '2',
-    name: 'Bodegal',
-    order: 141542,
-    date: '2014-12-12',
-    star: '08:30',
-    end: '09:30',
-    total_horas: '2.00'
-  },
-  {
-    id: '3',
-    name: 'Metrica',
-    order: 141543,
-    date: '2014-12-12',
-    star: '08:30',
-    end: '09:30',
-    total_horas: '2.00'
-  },
-  {
-    id: '4',
+    id: '46260b7b-a3a6-4023-a22c-2ab0a517e530',
+    horasTrabajadas: {
+      horas: 9,
+      minutos: 15,
+      extras: 0
+    },
     name: 'Ingesert',
-    order: 141544,
+    order: '141552',
     date: '2014-12-12',
-    star: '08:30',
-    end: '09:30',
-    total_horas: '2.00'
+    start: '08:30',
+    end: '17:30'
+  },
+  {
+    id: '86984826-4243-489a-b58d-44b12744c3d8',
+    horasTrabajadas: {
+      horas: 5,
+      minutos: 0,
+      extras: 0
+    },
+    name: 'Bodegal',
+    order: '141553',
+    date: '2014-12-12',
+    start: '09:30',
+    end: '14:30'
+  },
+  {
+    id: '8263d76b-1dc6-46c5-aea9-b601b09c6483',
+    horasTrabajadas: {
+      horas: 9,
+      minutos: 15,
+      extras: 0
+    },
+    name: 'Metrica',
+    order: '141554',
+    date: '2014-12-12',
+    start: '08:30',
+    end: '17:30'
   }
 ]
--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const app = express()
 
 app.use(cors({
   origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE']
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }))
 app.use(express.json())
 app.use('/api/v1', router)
 
 app.get('/', (req, res) => {
   res.status(200).send('Server is running')
+})
+
+app.all('*', (req, res) => {
+  res.status(404).send('404 Not Found')
 })
 
 const PORT = process.env.PORT || 3000
